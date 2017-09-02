75fc79f903de6417cb97b51a4c85ba40a42efc86
oscarotero <oom@oscarotero.com>
2017-09-02 18:34:43 +0200

    Code style fixer

--- tests/BasePathTest.php
+++ tests/BasePathTest.php
@@ -2,10 +2,10 @@
 
 namespace Middlewares\Tests;
 
-use PHPUnit\Framework\TestCase;
 use Middlewares\BasePath;
 use Middlewares\Utils\Dispatcher;
 use Middlewares\Utils\Factory;
+use PHPUnit\Framework\TestCase;
 
 class BasePathTest extends TestCase
 {
@@ -53,6 +53,10 @@ class BasePathTest extends TestCase
 
     /**
      * @dataProvider pathProvider
+     * @param mixed $basePath
+     * @param mixed $uri
+     * @param mixed $result
+     * @param mixed $location
      */
     public function testBasePath($basePath, $uri, $result, $location)
     {
